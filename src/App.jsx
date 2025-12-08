fad6b708a5c5c15f8a22878bd157b29b0ed7973c
anthony kingston <britdig2@gmail.com>
2025-12-08 18:12:00 +1000

    added strings folder to components

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,19 @@ import './App.scss';
 import'./components/Header.scss'
 import './index.scss';
 import Header from "./components/Header";
+import { projectStrings } from "./components/cardStrings";
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Header />
-      <div className="page-container">
-        <p1></p1>
-        
-      </div>
-      
-      
-      
-     
-    </>
+  <Header />
+
+  <div className="page-container">
+    <p>{projectStrings.LongText}</p>
+  </div>
+</>
+
   )
 }
 
